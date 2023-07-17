13f0fed02c7d7dc236e0caf5745ac02397977f84
MH01-Hasan <mehediph2021@gmail.com>
2023-07-17 23:59:33 +0600

    add user interface

---
+++ src/redux/features/user/userSlice.ts
@@ -0,0 +1,8 @@
+type iUserState = {
+  data: {
+    _id: string;
+    name: string;
+    email: string;
+  } | null;
+  isLoading: boolean;
+};
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  daisyui: {
+    themes: [],
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+};
