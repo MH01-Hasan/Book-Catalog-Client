33f56b5afe2c27aef1b27301b475902f5e18d359
MH01-Hasan <mehediph2021@gmail.com>
2023-07-18 00:08:43 +0600

    logout impliment

--- src/redux/features/user/userSlice.ts
+++ src/redux/features/user/userSlice.ts
@@ -1,3 +1,5 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
 type iUserState = {
   data: {
     _id: string;
@@ -6,3 +8,24 @@ type iUserState = {
   } | null;
   isLoading: boolean;
 };
+const initialState: iUserState = {
+  data: null,
+  isLoading: false,
+};
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setUser: (state, action: PayloadAction<iUserState["data"]>) => {
+      state.data = action.payload;
+      state.isLoading = false;
+    },
+    logout: (state) => {
+      state.data = null;
+      state.isLoading = false;
+      localStorage.removeItem("token");
+    },
+  },
+});
+
+export const { setUser, logout } = userSlice.actions;
